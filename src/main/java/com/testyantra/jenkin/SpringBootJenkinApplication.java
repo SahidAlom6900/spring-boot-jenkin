632c9d97488068ec7d0267c105586657b954c18c
sahidalom6900 <sahidalom6900@gmail.com>
2022-03-23 13:18:03 +0530

    first commit

---
+++ src/main/java/com/testyantra/jenkin/SpringBootJenkinApplication.java
@@ -0,0 +1,26 @@
+package com.testyantra.jenkin;
+
+import javax.annotation.PostConstruct;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class SpringBootJenkinApplication {
+	
+	public static final Logger LOGGER=LoggerFactory.getLogger(SpringBootJenkinApplication.class);
+	
+	@PostConstruct
+	public void init() {
+		LOGGER.info("Application Started....");
+	}
+
+	public static void main(String[] args) {
+		LOGGER.info("Application Executed....");
+		SpringApplication.run(SpringBootJenkinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/testyantra/jenkin/SpringBootJenkinApplicationTests.java
@@ -0,0 +1,21 @@
+package com.testyantra.jenkin;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+ class SpringBootJenkinApplicationTests {
+
+	public static final Logger LOGGER = LoggerFactory.getLogger(SpringBootJenkinApplicationTests.class);
+
+	@Test
+	 void contextLoads() {
+		LOGGER.info("Test Cases Started....");
+		assertEquals(true, true);
+	}
+
+}
